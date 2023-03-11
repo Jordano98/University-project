86487c1a87ae7da13c8767fb50e8261be6088885
Jordano98 <mohammad.dehghan218@gmail.com>
2023-03-11 22:57:34 +0330

    final part of edition

--- university/urls.py
+++ university/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import UniListView,UniDetailView
+from .views import UniListView,UniDetailView,uni_search
+
+app_name= 'university'
+
 urlpatterns = [
     path('',UniListView.as_view(),name = 'uni-list'),
     path('university/<int:pk>/',UniDetailView.as_view(),name = 'uni-detail'),
+    path('search/',uni_search,name="search"),
 ]
--- university/views.py
+++ university/views.py
@@ -1,16 +1,53 @@
 from django.shortcuts import render,redirect
 from django.views.generic import ListView,DetailView
+import django_filters
 from .models import University
 # Create your views here.
+
+class UniFilter(django_filters.FilterSet):
+    class Meta:
+        model = University
+        fields = ["city"]
+
 
 class UniListView(ListView):
     paginate_by = 2
     model = University
-    queryset = University.objects.all()
-    
     context_object_name = 'unis'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        filter = UniFilter(self.request.GET, queryset)
+        return filter.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        queryset = self.get_queryset()
+        filter = UniFilter(self.request.GET, queryset)
+        context["filter"] = filter
+        return context
+    
 
 class UniDetailView(DetailView):
     
     model = University
 
+    context_object_name = 'uni'
+
+
+def uni_search(request):
+    unis=University.objects.all()
+    if request.method == 'GET':
+        if x := request.GET.get('s') :
+            name = unis.filter(name__contains=x)
+            city = unis.filter(city__name__contains=x)
+            country = unis.filter(city__country__name__contains=x)
+            if name: 
+                unis=name
+            elif city:
+                unis = city
+            elif country:
+                unis =country
+            
+    context={'unis':unis }
+    return render(request,'university/university_list.html',context)
